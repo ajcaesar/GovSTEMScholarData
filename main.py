84a567250a3724aee62a36331a52fa38d1d2de86
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-02-22 11:25:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ import streamlit as st
 from io import StringIO
 
 df = pd.read_csv("Data Spreadsheet - Sheet1.csv")
+df1 = pd.DataFrame(columns = ["Week"
+df2 = 
+df3 = 
+df4 = 
+df5 = 
+df6 = 
+df7 = 
+df8 = 
+df9 =
+df10 = 
 st.write(df)
