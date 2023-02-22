4c8f6b4464b507c14fa0f2f24ecf6f598c313885
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-02-22 18:47:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import streamlit as st
 from io import StringIO
 
 df = pd.read_csv("Data Spreadsheet - Sheet1.csv")
-df1 = pd.DataFrame(columns = ["Week"
-df2 = 
-df3 = 
-df4 = 
-df5 = 
-df6 = 
-df7 = 
-df8 = 
-df9 =
-df10 = 
+df1 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df2 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df3 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df4 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df5 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df6 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df7 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df8 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df9 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df10 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
 st.write(df)
