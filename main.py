886a05fd8b048b2d5a531f70cbfce8d7f8f7543c
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-02-22 19:43:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import streamlit as st
 from io import StringIO
 
 df = pd.read_csv("Data Spreadsheet - Sheet1.csv")
-df1 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df2 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df3 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df4 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df5 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df6 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df7 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df8 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df9 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
-df10 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite"])
+df1 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df2 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df3 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df4 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df5 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df6 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df7 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df8 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df9 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
+df10 = pd.DataFrame(columns = ["Week", "pH", "Nitrate", "Nitrite", "Water Added"])
 st.write(df)
 
 i = 0; 
