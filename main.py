0a14ea8c1743af01b2ad3a3c672792a7560c84a7
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-03-10 14:08:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ while i < 10:
   i += 1
 
 option = st.selectbox(
-     'Pick your container',
+     'Pick your container/statistic',
      ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'pH', 'Nitrite', 'Nitrate'))
 
 if option == '1':
